123c3aeef1f10c73d194ec61398f6012f62cb0d1
Sebastian Wilzbach <seb@wilzba.ch>
2017-04-01 18:06:57 +0200

    switch to mobx

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
-import './App.css';
+
+//styles
+import './App.less';
+import './App.scss';
+import './App.styl';
+//import styles from './Modules.css';
+import HumanModel from './HumanModel';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <img src={logo} className="App-logo" alt="logo"/>
+          <h2 className="App-title"> ☢ custom-react-scripts ☢ </h2>
+          <div className="App-subtitle"> allow custom config for create-react-app without ejecting</div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <HumanModel></HumanModel>
       </div>
-    );
+    )
   }
 }
 
