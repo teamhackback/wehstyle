25fdda18767e3beffb76a68ae92a4efceb4f18c7
Sebastian Wilzbach <seb@wilzba.ch>
2017-04-01 18:12:48 +0200

    cleanup & move to folders

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
-
 //styles
-import './App.less';
-import './App.scss';
-import './App.styl';
-//import styles from './Modules.css';
-import HumanModel from './HumanModel';
+import HumanModel from './views/HumanModel';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <h2 className="App-title"> ☢ custom-react-scripts ☢ </h2>
-          <div className="App-subtitle"> allow custom config for create-react-app without ejecting</div>
-        </div>
         <HumanModel></HumanModel>
       </div>
     )
